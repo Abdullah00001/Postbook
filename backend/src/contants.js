145d6f81e0a506709434470fd6d22783054f07f7
abdullah00001 <abdullahbinomarchowdhury02@gmail.com>
2024-10-30 10:10:17 +0600

    add utility function for jwt and add constants variable

--- backend/src/contants.js
+++ backend/src/contants.js
@@ -1,4 +1,5 @@
 const baseUrl = 'api/v1';
 const dbName = 'postbook';
+const jwt
 
 export { baseUrl, dbName };
---
+++ backend/src/utils/jwt.utils.js
@@ -0,0 +1,48 @@
+import jwt from 'jsonwebtoken';
+
+const genareteAccessToken = playload => {
+  try {
+    const token = jwt.sign(playload, process.env.ACCESSTOKEN_SECRET_KEY);
+    return token;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+const generateRefreshToken = playload => {
+  try {
+    const token = jwt.sign(playload, process.env.REFRESHTOKEN_SECRET_KEY);
+    return token;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+const verifyAccessToken = token => {
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESSTOKEN_SECRET_KEY);
+    return decoded;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+const verifyRefreshToken = token => {
+  try {
+    const decoded = jwt.verify(token, process.env.REFRESHTOKEN_SECRET_KEY);
+    return decoded;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export default {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+};
